4b3ff7dc482cfef925b1c6edfe511db2e979bd11
Antony Messerli <amesserl@rackspace.com>
2018-03-26 12:35:17 -0500

    Add travis.yml for generating bootloaders

--- bootloader/ipxe/configs/general.h
+++ bootloader/ipxe/configs/general.h
@@ -1,6 +1,5 @@
 #define DIGEST_CMD            /* Image crypto digest commands */
 #define DOWNLOAD_PROTO_HTTPS  /* Secure Hypertext Transfer Protocol */
-#define IMAGE_COMBOOT         /* COMBOOT */
 #define IMAGE_TRUST_CMD	      /* Image trust management commands */
 #define NET_PROTO_IPV6        /* IPv6 protocol */
 #define NSLOOKUP_CMD          /* DNS resolving command */
